dedaf250f187a64a4e4fddde0acc05ffbe93f392
JJ Style <style.jj@pm.me>
2021-10-02 21:40:25 +0100

    extend scrape and add jobs to DB

--- src/storage/interface.go
+++ src/storage/interface.go
@@ -1,6 +1,10 @@
 package storage
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 // Job is, for now, an indeedJob
 type Job IndeedJob
@@ -16,6 +20,18 @@ type IndeedJob struct {
 	Location     string `gorm:"not null"`
 	Company      string
 	CompanyImage string
+}
+
+func (j *Job) FromIndeedResponse(resp map[string]interface{}) error {
+	// TODO(jj) - add type check validation
+	j.Title = fmt.Sprint(resp["positionName"])
+	j.Company = fmt.Sprint(resp["company"])
+	j.Location = fmt.Sprint(resp["location"])
+	j.IndeedURL = fmt.Sprint(resp["url"])
+	j.IndeedID = fmt.Sprint(resp["id"])
+	j.Description = fmt.Sprint(resp["description"])
+	j.Metadata = fmt.Sprint(resp["metadata"])
+	return nil
 }
 
 type JobDatabase interface {
